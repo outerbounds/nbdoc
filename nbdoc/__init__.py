9d6aa6207bb37ba0834b979343744a44ec8f3d63
hamelsmu <hamel.husain@gmail.com>
2022-02-23 08:28:27 -0800

    enforce py 3.9

--- nbdoc/__init__.py
+++ nbdoc/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.0.17"
+__version__ = "0.0.18"
+
+
+import sys
+_pv = sys.version_info
+if _pv < (3, 9):
+    raise SystemError(f'nbdoc requires python version 3.9.0 or higher. You are running {_pv.major}.{_pv.minor}.{_pv.micro}')
